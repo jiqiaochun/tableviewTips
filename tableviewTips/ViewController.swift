603158066ae099b91b69be9cbbbeaa69121a5419
qiaochun ji <qiaochunji@jiqiaochun-macbook-pro.local>
2018-07-11 10:11:41 +0800

    Initial Commit

---
+++ tableviewTips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableviewTips
+//
+//  Created by qiaochun ji on 2018/7/11.
+//  Copyright © 2018年 qiaochun ji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
